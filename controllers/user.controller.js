c9aba106bb1ad4718103f2bfeb3d67d12cc6d11d
regularvanessaperson <vamavila4@gmail.com>
2020-12-26 15:25:02 -0600

    testing done for userProfile, able to access user successfully

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,10 +12,12 @@ exports.adminBoard = (req, res) => {
     res.status(200).send("Admin content")
 }
 
+//display user profile
 exports.userProfile = (req, res) => {
     const id= req.body._id
     User.find({_id: id}).then((user)=>{
         if(!user)
-        return res.status(400).send({message: })
+        return res.status(400).send({message: "Profile not found" })
+        else res.send(user)
     })
 }
